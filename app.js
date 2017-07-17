52c89323ffaef93796d2623dd7363335c763fa1e
Christian Sedlacek <csedlacek@krose-iMac.local>
2017-07-17 10:23:18 -0500

    Initial module public interface, skeleton of protected functions added; documentation for functions added

---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+	Module that fetches data from a Product Reviews API and writes it to JSON files
+	@module reviews-to-json
+*/
+
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+
+/**
+	Fetch review average for product(s)
+	@protected
+	@param {Array} reviews Array of product reviews to calculate the average with
+	@returns {Number} Numeric average of all reviews passed to function
+*/
+ calculateProductAverage = (reviews) => {
+
+}
+
+module.exports = {
+	/**
+		Fetch reviews for product(s) and write them to one JSON file per product
+		@param {Array} skus Array of SKUs, one for each product to fetch
+		@param {Object} config Config object
+		@param {string} config.outputDir Directory to write output files to
+		@param {string} config.apiKey API private key
+		@returns {Promise.<boolean|Error>} Promise resolving to true if fetch/write was a success, resolves to an error upon failure
+	*/
+	fetchProductReviews: (skus, {outputDir, apiKey}) => {
+
+	}
+};
