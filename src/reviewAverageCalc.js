e987a220eb8a713df47477067e900c29aef6921c
Christian Sedlacek <csedlacek@krose-iMac.local>
2017-07-17 12:37:09 -0500

    Fixed calculateProductAverage to not break when number passed in is in string form

--- src/reviewAverageCalc.js
+++ src/reviewAverageCalc.js
@@ -5,13 +5,14 @@ module.exports = {
 		@returns {Number} Numeric average of all reviews passed to function
 	*/
 	 calculateProductAverage: (reviews) => {
-	 	let average = 0;
+	 	let total = 0;
 	 	for (review of reviews) {
-	 		if (!review.productRating || isNaN(review.productRating)) {
+	 		const rating = parseInt(review.productRating);
+	 		if (!review.productRating || isNaN(rating)) {
 	 			throw new Error('All reviews must have an integer productRating property specified. instead found '+review.productRating);
 	 		}
-	 		average += review.productRating;
+	 		total += rating;
 	 	}
-	 	return (average / reviews.length);
+	 	return (total / reviews.length);
 	}
 }
