1b90d44f268e17cd9623861ccb909535ae63d84c
Christian Sedlacek <csedlacek@krose-iMac.local>
2017-07-17 10:40:22 -0500

    Added documentation example for fetchProductReviews method

--- app.js
+++ app.js
@@ -22,12 +22,22 @@ module.exports = {
 		Fetch reviews for product(s) and write them to one JSON file per product
 		@param {Array.<string>} skus Array of SKUs, one for each product to fetch
 		@param {Object} config Config object
-		@param {string} config.outputDir Directory to write output files to
+		@param {string} [config.outputDir='./products'] Directory to write output files to (optional, defaults to './products')
 		@param {string} config.apiUrl API base URL
 		@param {string} config.apiKey API private key
 		@returns {Promise.<boolean|Error>} Promise resolving to true if fetch/write was a success, resolves to an error upon failure
+		@example
+		// fetches product reviews for products 'B6101W', 'B3101W', 'B3300W' and writes them to one JSON file per product in the directory './productReviews'
+		// returns a promise that should resolve to true (assuming the API responds successfully)
+		reviewsToJson.fetchProductReviews([
+			'B6101W', 'B3101W', 'B3300W'
+		],
+		{
+			outputDir: './productReviews',
+			apiUrl: 'https://my-reviews-api.herokuapp.com',
+			apiKey: 'MY_API_KEY'
+		});
 	*/
-	fetchProductReviews: (skus, {outputDir, apiKey, apiUrl}) => {
-
+	fetchProductReviews: (skus, {outputDir = './products', apiKey, apiUrl}) => {
 	}
 };
