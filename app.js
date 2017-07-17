fa8415e08f64373388f8bd8117073a26a75dbac0
Christian Sedlacek <csedlacek@krose-iMac.local>
2017-07-17 10:26:28 -0500

    Function interface documentation updates

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('isomorphic-fetch');
 /**
 	Fetch review average for product(s)
 	@protected
-	@param {Array} reviews Array of product reviews to calculate the average with
+	@param {Array.<Object>} reviews Array of product reviews to calculate the average with
 	@returns {Number} Numeric average of all reviews passed to function
 */
  calculateProductAverage = (reviews) => {
@@ -20,13 +20,14 @@ require('isomorphic-fetch');
 module.exports = {
 	/**
 		Fetch reviews for product(s) and write them to one JSON file per product
-		@param {Array} skus Array of SKUs, one for each product to fetch
+		@param {Array.<string>} skus Array of SKUs, one for each product to fetch
 		@param {Object} config Config object
 		@param {string} config.outputDir Directory to write output files to
+		@param {string} config.apiUrl API base URL
 		@param {string} config.apiKey API private key
 		@returns {Promise.<boolean|Error>} Promise resolving to true if fetch/write was a success, resolves to an error upon failure
 	*/
-	fetchProductReviews: (skus, {outputDir, apiKey}) => {
+	fetchProductReviews: (skus, {outputDir, apiKey, apiUrl}) => {
 
 	}
 };
