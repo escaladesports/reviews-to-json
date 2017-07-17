6e5ae2fb6ad8d8de7dfcd6c6de2ff0fc031b470b
Christian Sedlacek <csedlacek@krose-iMac.local>
2017-07-17 11:03:28 -0500

    Added reviewAverageCalc behavior and unit tests

--- src/reviewAverageCalc.js
+++ src/reviewAverageCalc.js
@@ -5,6 +5,13 @@ module.exports = {
 		@returns {Number} Numeric average of all reviews passed to function
 	*/
 	 calculateProductAverage: (reviews) => {
-
+	 	let average = 0;
+	 	for (review of reviews) {
+	 		if (!review.productRating || isNaN(review.productRating)) {
+	 			throw new Error('All reviews must have an integer productRating property specified. instead found '+review.productRating);
+	 		}
+	 		average += review.productRating;
+	 	}
+	 	return (average / reviews.length);
 	}
 }
---
+++ test/reviewAverageCalc.js
@@ -0,0 +1,51 @@
+// force test mode
+process.env.NODE_ENV = 'test';
+
+// dependencies
+const chai = require('chai');
+const reviewAverageCalc = require('../src/reviewAverageCalc.js');
+
+// chai setup
+const should = chai.should();
+const expect = chai.expect;
+
+// test suite
+describe('reviewAverageCalc', () => {
+	it('should calculate an average based on main rating assigned to each product review', done => {
+		const reviews = [
+			{productRating: 5},
+			{productRating: 3},
+			{productRating: 1},
+			{productRating: 4},
+			{productRating: 5}
+		];
+		const expectAverage = 3.6;
+		const testAverage = reviewAverageCalc.calculateProductAverage(reviews);
+		testAverage.should.equal(expectAverage);
+		done();
+	});
+
+	it('should throw an error if review(s) don\'t have productRating specified', done => {
+		const reviews = [
+			{productRating: 5},
+			{productRating: 3},
+			{productRating: 1},
+			{productRating: 4},
+			{} // missing productRating!
+		];
+		expect(() => reviewAverageCalc.calculateProductAverage(reviews)).to.throw(Error);
+		done();
+	});
+
+	it('should throw an error if any review\'s productRating is not a number', done => {
+		const reviews = [
+			{productRating: 5},
+			{productRating: 3},
+			{productRating: 1},
+			{productRating: 4},
+			{productRating: 'I am a string!'}
+		];
+		expect(() => reviewAverageCalc.calculateProductAverage(reviews)).to.throw(Error);
+		done();
+	});
+});
